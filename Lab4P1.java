b4fc947d9e826e67610b3a99b9ed695cc783aded
Satvi27-debug <satvendragirinihal@gmail.com>
2025-10-15 10:33:53 +0530

    Add files via upload

---
+++ Lab4P1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Lab4P1 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x, y;
+        try
+        {
+            x = s.nextInt();
+            y = s.nextInt();
+            System.out.println(x/y);
+        }
+        catch(InputMismatchException ime)
+        {
+            System.out.println(ime.getClass().getName());
+        }
+        catch(ArithmeticException ae)
+        {
+            System.out.println(ae.getClass().getName() + ": / by zero");
+        }
+    }
+}
